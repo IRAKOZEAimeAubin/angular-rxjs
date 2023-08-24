5a2e9d2177093b75a9fd22f18dfd22da43b3a5d3
Irakoze Aime Aubin <irakozeaimeaubin@gmail.com>
2023-08-24 17:58:14 +0200

    commit #2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { from, map, of, take, tap } from 'rxjs';
 
 @Component({
   selector: 'ra-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'rxjs-angular';
+export class AppComponent implements OnInit {
+  name = `Angular ${VERSION.major}`;
+  ngOnInit() {
+    // of([2, 3, 5, 7]).subscribe({
+    //   next: (item) => console.log(`resulting item: ${item}`),
+    //   error: (err) => console.error(`Error: ${err}`),
+    //   complete: () => console.log(`--No more items, Go Home!--`),
+    // });
+    // from([2, 3, 5, 7]).subscribe({
+    //   next: (item) => console.log(`resulting item: ${item}`),
+    //   error: (err) => console.error(`Error: ${err}`),
+    //   complete: () => console.log(`--No more items, Go Home!--`),
+    // });
+    // of(...[2, 3, 5, 7]).subscribe({
+    //   next: (item) => console.log(`resulting item: ${item}`),
+    //   error: (err) => console.error(`Error: ${err}`),
+    //   complete: () => console.log(`--No more items, Go Home!--`),
+    // });
+
+    of(2, 3, 5, 7)
+      .pipe(
+        map((item) => item * 2),
+        // tap((item) => console.log(item))
+        take(2)
+      )
+      .subscribe({
+        next: (item) => console.log(item),
+      });
+  }
 }
